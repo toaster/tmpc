3d1d5a1d6fa1ff7a634178fcb7e6c7d69d6cab26
Tilo Prütz <tilo@pruetz.net>
2020-07-26 11:23:18 +0200

    improve icon generator script style

--- internal/ui/icons/gen.sh
+++ internal/ui/icons/gen.sh
@@ -1,35 +1,39 @@
 #!/bin/sh -ex
 
-DIR=`dirname "$0"`
+DIR=$(dirname "$0")
 STATIC_FILE="../generated_icons_static.go"
 THEMED_FILE="../generated_icons_themed.go"
 PKG="ui"
-fyne=`go env GOPATH`/bin/fyne
+fyne=$(go env GOPATH)/bin/fyne
 
-cd $DIR
+cd "$DIR"
 rm -f $STATIC_FILE
 rm -f $THEMED_FILE
 
 append=""
 for file in *.svg
 do
-  name=`basename $file .svg | perl -pe 's#(^|-|_)([a-z])#\u$2#g'`
-  test -z "$append" && {
-    $fyne bundle -package $PKG -name rsc$name $file > $STATIC_FILE
-    echo "// auto-generated" > $THEMED_FILE
-    echo >> $THEMED_FILE
-    echo "package $PKG" >> $THEMED_FILE
-    echo >> $THEMED_FILE
-    echo 'import "fyne.io/fyne/theme"' >> $THEMED_FILE
-  } || {
-    $fyne bundle -package $PKG -name rsc$name -append $file >> $STATIC_FILE
-  }
-  echo >> $THEMED_FILE
-  echo "// ${name}Icon is a themed version of the ${name} resource." >> $THEMED_FILE
-  echo "var ${name}Icon *theme.ThemedResource" >> $THEMED_FILE
-  echo >> $THEMED_FILE
-  echo "func init() {" >> $THEMED_FILE
-  echo "\t${name}Icon = theme.NewThemedResource(rsc${name}, nil)" >> $THEMED_FILE
-  echo "}" >> $THEMED_FILE
+  name=$(basename "$file" .svg | perl -pe 's#(^|-|_)([a-z])#\u$2#g')
+  if test -z "$append"; then
+    $fyne bundle -package $PKG -name "rsc$name" "$file" > $STATIC_FILE
+    {
+      echo "// auto-generated"
+      echo
+      echo "package $PKG"
+      echo
+      echo 'import "fyne.io/fyne/theme"'
+    } > $THEMED_FILE
+  else
+    $fyne bundle -package $PKG -name "rsc$name" -append "$file" >> $STATIC_FILE
+  fi
+  {
+    echo
+    echo "// ${name}Icon is a themed version of the ${name} resource."
+    echo "var ${name}Icon *theme.ThemedResource"
+    echo
+    echo "func init() {"
+    printf "\t%sIcon = theme.NewThemedResource(rsc%s, nil)\n" "$name" "$name"
+    echo "}"
+  } >> $THEMED_FILE
   append="yes"
 done
