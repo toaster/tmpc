5a783f24ee81493e285aba4c8c82ecb8f8c31e8c
Tilo Prütz <tilo@pruetz.net>
2023-10-31 22:03:42 +0100

    [metadata] avoid double escape in regexp value

--- internal/metadata/metadata.go
+++ internal/metadata/metadata.go
@@ -159,12 +159,12 @@ var titleReplacers map[string]*strings.Replacer
 
 func init() {
 	generalTitleReplacer = strings.NewReplacer("‐", "", "-", "", "  ", " ", "'", "", "’", "", "‘", "")
-	numRegexp = regexp.MustCompile("\\b\\d+\\b")
+	numRegexp = regexp.MustCompile(`\b\d+\b`)
 	numReplacements = map[string]map[string]string{
 		"de": {"0": "null", "1": "eins", "2": "zwei", "3": "drei", "4": "vier", "5": "fuenf", "6": "sechs", "7": "sieben", "8": "acht", "9": "neun", "10": "zehn", "11": "elf", "12": "zwoelf"},
 		"en": {"0": "zero", "1": "one", "2": "two", "3": "three", "4": "four", "5": "five", "6": "six", "7": "seven", "8": "eight", "9": "nine", "10": "ten", "11": "eleven", "12": "twelve"},
 	}
-	reduceRegexp = regexp.MustCompile("\\(.*\\)")
+	reduceRegexp = regexp.MustCompile(`\(.*\)`)
 	titleReplacers = map[string]*strings.Replacer{
 		"de": strings.NewReplacer("ä", "ae", "ö", "oe", "ü", "ue", "ß", "ss", "&", "und"),
 		"en": strings.NewReplacer("&", "and"),
